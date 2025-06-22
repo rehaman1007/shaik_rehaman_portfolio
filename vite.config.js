0230af2fe682262725bf67b3e09f90a982091058
Pitta Sravani <sravanipitta7013@gmail.com>
2025-06-22 18:07:49 +0530

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/shaik_rehaman_portfolio/',
   plugins: [react()],
   server: {
       port: 7777
